a01dcd2d51b33231cd2191b2ac633ae94e574b65
Arun__im <arunim2810@gmail.com>
2026-04-11 14:47:08 +0530

    Docker Updated File

--- linkforge/src/test/java/com/arun/linkforge/LinkforgeApplicationTests.java
+++ linkforge/src/test/java/com/arun/linkforge/LinkforgeApplicationTests.java
@@ -2,8 +2,10 @@ package com.arun.linkforge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LinkforgeApplicationTests {
 
 	@Test
